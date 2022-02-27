6788d0d8adb5573f795d1dc17361219e519d1205
Nikitakorovinskii <78597438+Nikitakorovinskii@users.noreply.github.com>
2022-02-27 17:16:36 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <KorovinskiiComponent></KorovinskiiComponent>
+   <Vyatsu :results="results"></Vyatsu>
+   <Regestration></Regestration>
+</template>
+
+<script>
+import Regestration from 'regestration';
+import axios from 'axios';
+import KorovinskiiComponent from 'korovinskii-component';
+import Vyatsu from 'vytsu';
+
+const url = 'http://localhost:8080/admission/';
+
+export default {
+  components: {
+    KorovinskiiComponent,
+    Vyatsu,
+    Regestration,
+  },
+  data() {
+    return { KorovinskiiComponent, results: [] };
+  },
+  mounted() {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response);
+        this.results = response.data;
+      })
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style>
+</style>
